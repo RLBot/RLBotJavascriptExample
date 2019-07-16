b60a6b4c5484b91aedbf3a553eead32eba13ee7f
SuperVK <victor.klomp@gmail.com>
2019-07-16 15:43:35 +0200

    added rendering

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const { BaseAgent, SimpleController, quickChats, Manager } = require('rlbot-test');
-const { GameState, BallState, CarState, Physics, Vector3 } = require('rlbot-test').gameStateUtil
+const { GameState, BallState, CarState, Physics, Vector3 } = require('rlbot-test').GameStateUtil
 
 class ATBA extends BaseAgent {
     constructor(name, team, index, fieldInfo) {
-        super(name, team, index, fieldInfo) //pushes these all to this.
+        super(name, team, index, fieldInfo) //pushes these all to `this`.
     }
     getOutput(gameTickPacket, ballPrediction) {
         var controller = new SimpleController()
@@ -15,6 +15,7 @@ class ATBA extends BaseAgent {
         var ballLocation = gameTickPacket.ball.physics.location;
         var carLocation = gameTickPacket.players[this.index].physics.location;
         var carRotation = gameTickPacket.players[this.index].physics.rotation;
+        
 
         // Calculate to get the angle from the front of the bot's car to the ball.
         var botToTargetAngle = Math.atan2(ballLocation.y - carLocation.y, ballLocation.x - carLocation.x);
@@ -31,6 +32,18 @@ class ATBA extends BaseAgent {
             controller.steer = -1;
         }
         
+        this.renderer.beginRendering()
+        this.renderer.drawString2D(20, 20, 3, 3, 'Hello world', new this.renderer.Color(255, 255, 0, 0))
+        
+        let black = new this.renderer.Color(255, 0, 0, 0)
+        for(let i = 0; i < ballPrediction.slices.length-1; i++) {
+            let ball1 = ballPrediction.slices[i].physics.location
+            let ball2 = ballPrediction.slices[i+1].physics.location
+            this.renderer.drawLine3D(new Vector3(ball1.x, ball1.y, ball1.z), new Vector3(ball2.x, ball2.y, ball2.z), black)
+        }
+        
+        this.renderer.endRendering()
+
         //almost scored
         if(ballPrediction.slices[10].physics.location.y > 5120 || ballPrediction.slices[10].physics.location.y < -5120) {
             this.sendQuickChat(quickChats.compliments.NiceShot, false)
@@ -42,11 +55,12 @@ class ATBA extends BaseAgent {
             this.setGameState(new GameState(ball))
         }
         //cancel gravity
-        let cars = []
-        for(let car of gameTickPacket.players) {
-            cars.push(new CarState(new Physics(null, null, new Vector3(null, null, car.physics.velocity.z+(650/55)))))
-        }
-        this.setGameState(new GameState(null, cars))
+        // let cars = []
+        // for(let car of gameTickPacket.players) {
+        //     cars.push(new CarState(new Physics(null, null, new Vector3(null, null, car.physics.velocity.z+(650/55)))))
+        // }
+        // let ball = new BallState(new Physics(null, null, new Vector3(null, null, gameTickPacket.ball.physics.velocity.z+(650/55))))
+        // this.setGameState(new GameState(ball, cars))
         controller.throttle = 1;
         return controller;
 
